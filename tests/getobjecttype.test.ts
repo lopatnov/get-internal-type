fdef758a0233a999a4e511b6a0562d5ea8f0f3f0
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2019-10-23 21:24:20 +0300

    Added Primitive types tests

--- tests/getobjecttype.test.ts
+++ tests/getobjecttype.test.ts
@@ -1,28 +1,97 @@
 import getInternalType from "../src/get-internal-type";
 
-describe("Primitive tests", () => {
-  it("should get type of boolean false", () => {
+describe("Primitive types tests", () => {
+  it("should get type of undefined", () => {
+    let actual = getInternalType(undefined);
+    let expected = "undefined";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of null", () => {
+    let actual = getInternalType(null);
+    let expected = "null";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of the boolean value false", () => {
     let actual = getInternalType(false);
     let expected = "boolean";
 
     expect(actual).toBe(expected);
   });
-  it("should get type of boolean true", () => {
+  it("should get type of the boolean value true", () => {
     let actual = getInternalType(true);
     let expected = "boolean";
 
     expect(actual).toBe(expected);
   });
-  it("should get type of empty string", () => {
+  it("should get type of the empty string", () => {
     let actual = getInternalType('');
     let expected = "string";
 
     expect(actual).toBe(expected);
   });
-  it("should get type of string", () => {
+  it("should get type of a string", () => {
     let actual = getInternalType('Some string');
     let expected = "string";
 
     expect(actual).toBe(expected);
+  });
+  it("should get type of the number zero", () => {
+    let actual = getInternalType(0);
+    let expected = "number";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of a number", () => {
+    let actual = getInternalType(123);
+    let expected = "number";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of the NAN", () => {
+    let actual1 = getInternalType(NaN);
+    let actual2 = getInternalType(Number.NaN);
+    let expected = "number";
+
+    expect(actual1).toBe(expected);
+    expect(actual2).toBe(expected);
+  });
+  it("should get type of a special numbers", () => {
+    let actual1 = getInternalType(Number.NaN);
+    let actual2 = getInternalType(Number.EPSILON);
+    let actual3 = getInternalType(Number.POSITIVE_INFINITY);
+    let actual4 = getInternalType(Number.NEGATIVE_INFINITY);
+    let actual5 = getInternalType(Number.MAX_SAFE_INTEGER);
+    let actual6 = getInternalType(Number.MIN_SAFE_INTEGER);
+    let actual7 = getInternalType(Number.MAX_VALUE);
+    let actual8 = getInternalType(Number.MIN_VALUE);
+    let expected = "number";
+
+    expect(actual1).toBe(expected);
+    expect(actual2).toBe(expected);
+    expect(actual3).toBe(expected);
+    expect(actual4).toBe(expected);
+    expect(actual5).toBe(expected);
+    expect(actual6).toBe(expected);
+    expect(actual7).toBe(expected);
+    expect(actual8).toBe(expected);
+  });
+  it("should get type of a BigInt", () => {
+    let bi = BigInt(9007199254740991);
+    let actual = getInternalType(bi);
+    let expected = "bigint";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of a symbol", () => {
+    let actual1 = getInternalType(Symbol());
+    let actual2 = getInternalType(Symbol(''));
+    let actual3 = getInternalType(Symbol('Test'));
+    let expected = "symbol";
+
+    expect(actual1).toBe(expected);
+    expect(actual2).toBe(expected);
+    expect(actual3).toBe(expected);
   });
 });
