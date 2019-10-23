120c5342bc4e225c42c9b3565fa5332472c01138
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2019-10-23 20:26:30 +0300

    Added tests for boolean and string

--- tests/getobjecttype.test.ts
+++ tests/getobjecttype.test.ts
@@ -1,6 +1,24 @@
 import getInternalType from "../src/get-internal-type";
 
 describe("Primitive tests", () => {
+  it("should get type of boolean false", () => {
+    let actual = getInternalType(false);
+    let expected = "boolean";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of boolean true", () => {
+    let actual = getInternalType(true);
+    let expected = "boolean";
+
+    expect(actual).toBe(expected);
+  });
+  it("should get type of empty string", () => {
+    let actual = getInternalType('');
+    let expected = "string";
+
+    expect(actual).toBe(expected);
+  });
   it("should get type of string", () => {
     let actual = getInternalType('Some string');
     let expected = "string";
